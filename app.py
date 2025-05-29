f0ee1d4f726d41b95d07e1f05b345cd26f184c1f
shishirsb <bapatshishir92@gmail.com>
2025-05-29 16:55:59 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,40 @@
+import json
+import ijson
+import csv
+techcorp = []
+healthcare = []
+
+def process_file():
+    with open('json_data', 'rb') as f:
+        for item in ijson.items(f, 'contacts.item'):
+            if item["digital_footprint"]["emails"][0]["domain"] == "techcorp.ai":
+                techcorp.append(
+                    [
+                        item["contact_id"], item["identity"]["name"]["first"], item["identity"]["name"]["last"],
+                        item["digital_footprint"]["emails"][0]["address"], item["professional"]["current"]["position"]["title"],
+                        item["professional"]["current"]["company"]["name"]
+                    ])
+            elif item["digital_footprint"]["emails"][0]["domain"] == 'healthtech.org':
+                healthcare.append(
+                    [
+                        item["contact_id"], item["identity"]["name"]["first"], item["identity"]["name"]["last"],
+                        item["digital_footprint"]["emails"][0]["address"],
+                        item["professional"]["current"]["position"]["title"],
+                        item["professional"]["current"]["company"]["name"]
+                    ])
+
+process_file()
+print(techcorp)
+print(healthcare)
+
+with open('csv_file_techcorp', 'w', newline = '', encoding='utf-8') as file:
+    writer = csv.writer(file)
+    for item in techcorp:
+        writer.writerow(item)
+
+with open('csv_file_HT', 'w', newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
+    for item in healthcare:
+        writer.writerow(item)
+
+
